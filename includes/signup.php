dd5f2ea33d2953c7ffbd55bdfa104eaad821800e
Afsan <afsan.haque@g.bracu.ac.bd>
2023-04-02 02:51:33 +0600

    gg

--- includes/signup.php
+++ includes/signup.php
@@ -26,58 +26,65 @@ if (isset($_POST['submit'])) {
 ?>
 <!--Javascript for check email availabilty-->
 <script>
-	function checkAvailability() {
+function checkAvailability() {
 
-		$("#loaderIcon").show();
-		jQuery.ajax({
-			url: "check_availability.php",
-			data: 'emailid=' + $("#email").val(),
-			type: "POST",
-			success: function(data) {
-				$("#user-availability-status").html(data);
-				$("#loaderIcon").hide();
-			},
-			error: function() {}
-		});
-	}
+    $("#loaderIcon").show();
+    jQuery.ajax({
+        url: "check_availability.php",
+        data: 'emailid=' + $("#email").val(),
+        type: "POST",
+        success: function(data) {
+            $("#user-availability-status").html(data);
+            $("#loaderIcon").hide();
+        },
+        error: function() {}
+    });
+}
 </script>
 
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-	<div class="modal-dialog" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-			</div>
-			<section>
-				<div class="modal-body modal-spa">
-					<div class="login-grids">
-						<div class="login">
-							<div class="login-left">
-								<ul>
-									<li><a class="fb" href="#"><i></i>Facebook</a></li>
-									<li><a class="goog" href="#"><i></i>Google</a></li>
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                        aria-hidden="true">&times;</span></button>
+            </div>
+            <section>
+                <div class="modal-body modal-spa">
+                    <div class="login-grids">
+                        <div class="login">
+                            <div class="login-left">
+                                <ul>
+                                    <li><a class="fb" href="#"><i></i>Facebook</a></li>
+                                    <li><a class="goog" href="#"><i></i>Google</a></li>
 
-								</ul>
-							</div>
-							<div class="login-right">
-								<form name="signup" method="post">
-									<h3>Create your account </h3>
+                                </ul>
+                            </div>
+                            <div class="login-right">
+                                <form name="signup" method="post">
+                                    <h3>Create your account </h3>
 
 
-									<input type="text" value="" placeholder="Full Name" name="fname" autocomplete="off" required="">
-									<input type="text" value="" placeholder="Mobile number" maxlength="10" name="mobilenumber" autocomplete="off" required="">
-									<input type="text" value="" placeholder="Email id" name="email" id="email" onBlur="checkAvailability()" autocomplete="off" required="">
-									<span id="user-availability-status" style="font-size:12px;"></span>
-									<input type="password" value="" placeholder="Password" name="password" required="">
-									<input type="submit" name="submit" id="submit" value="CREATE ACCOUNT">
-								</form>
-							</div>
-							<div class="clearfix"></div>
-						</div>
-						<p>By logging in you agree to our <a href="page.php?type=terms">Terms and Conditions</a> and <a href="page.php?type=privacy">Privacy Policy</a></p>
-					</div>
-				</div>
-			</section>
-		</div>
-	</div>
+                                    <input type="text" style="color:black" value="" placeholder="Full Name" name="fname"
+                                        autocomplete="off" required="" pattern="[A-Za-z]+">
+                                    <input type="text" style="color:black" value="" placeholder="Mobile number"
+                                        maxlength="11" name="mobilenumber" autocomplete="off" required=""
+                                        pattern="\d{11}" maxlength="15">
+                                    <input type="text" style="color:black" value="" placeholder="Email id" name="email"
+                                        id="email" onBlur="checkAvailability()" autocomplete="off" required=""
+                                        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$">
+                                    <span id="user-availability-status" style="font-size:12px;"></span>
+                                    <input type="password" value="" placeholder="Password" name="password" required="">
+                                    <input type="submit" name="submit" id="submit" value="CREATE ACCOUNT">
+                                </form>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                        <p>By logging in you agree to our <a href="page.php?type=terms">Terms and Conditions</a> and <a
+                                href="page.php?type=privacy">Privacy Policy</a></p>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
 </div>
